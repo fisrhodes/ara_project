56b67669fd4f8403d7976dd351b87653c83507ff
mohamadOsama-fis <mohammed.ramdan@fis.ps>
2021-09-16 13:13:50 +0300

    update app logo and luncher icon

--- lib/views/shared/utils/app_images.dart
+++ lib/views/shared/utils/app_images.dart
@@ -1,5 +1,5 @@
 class AppImages {
-  static const String araLogo = 'assets/images/ara.png';
+  static const String araLogo = 'assets/images/luncher.png';
   static const String microsoft = 'assets/images/microsoft.png';
   static const String adjust = 'assets/images/adjust.png';
 }
